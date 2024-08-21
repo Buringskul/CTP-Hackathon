23f153537da494f7e44b3c1a7c56c2ea9285f068
Johnathansanchez16 <Johnathansanchez16@gmail.com>
2024-08-21 14:19:31 -0400

    removed contact from header and added margin

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,7 +9,6 @@ function Header() {
         <ul className="nav-list">
           <li><a href="/">Home</a></li>
           <li><a href="/resources">Resources</a></li>
-          <li><a href="/contact">Contact</a></li>
           <li><a href="/signin">Sign in</a></li>
           <li><a href="/register">Register</a></li>
         </ul>
