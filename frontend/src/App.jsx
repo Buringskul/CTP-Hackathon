4e8105d28353ff3c21db5d3df65db7b97063d26f
Buringskul <simonaisakova6@gmail.com>
2024-08-21 15:54:11 -0400

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import MainContent from './components/MainContent'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MainContent from './components/MainContent';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<MainContent />} />
+          {/* You can add more Routes for other pages here */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
