65540dcefaf57a28359ce87802129b6d32af0a35
alyliann <92405765+alyliann@users.noreply.github.com>
2024-08-21 12:35:27 -0400

    Created User class with variables

--- crucialcircle.py
+++ crucialcircle.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import (Flask, flash, session)
 from flask_behind_proxy import FlaskBehindProxy
 from flask_sqlalchemy import SQLAlchemy
 from forms import SignUpForm, SignInForm
@@ -9,14 +9,49 @@ proxied = FlaskBehindProxy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///userinfo.db'
 
 db = SQLAlchemy(app)
+global logged_in
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(), primary_key=True)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    saved_recipes = db.Column(db.String(), nullable=True)
 
-    def __repr__(self):
-        return f'{self.name}', '{self.email}'
+#    def __repr__(self):
+#        return f'{self.name}', '{self.email}'
+
+
+def home(): # on home page
+    if 'email' in session:
+        user = User.query.filter_by(email=session['email']).first()
+        logged_in = True
+#        return render_template('home.html', user=user, logged_in=True)
+    else:
+        logged_in = False
+#        return render_template('home.html', logged_in=False)
+    
+
+def register(): # on register page
+    signup = SignUpForm()
+    signin = SignInForm()
+    logged_in = False
+
+    if signup.validate_on_submit():
+        existing_user = User.query.filter_by(email=signup.email.data).first()
+        if existing_user:
+            flash('Email already exists. Please use a different email.')
+            return redirect(url_for('register'))
+        else:
+            user = User(name=signup.name.data,
+                        email=signup.email.data,
+                        password=signup.password.data,
+                        saved_recipes='')
+            logged_in = True
+            db.session.add(user)
+            db.session.commit()
+            flash(f'Account created for {signup.name.data}!')
+            session['email'] = signup.email.data
+#            return redirect(url_for('home'))
+        
