d75761273cf25da50b718c92c9804dc78259dc5c
alyliann <92405765+alyliann@users.noreply.github.com>
2024-08-22 13:57:24 -0400

    commented out unnecessary code

--- backend/crucialcircle.py
+++ backend/crucialcircle.py
@@ -31,22 +31,22 @@ class Post():
     comments = []
 
 
-@app.route('/')
-@app.route('/home')
-def home():
-    if 'email' in session:
-        user = User.query.filter_by(email=session['email']).first()
-        logged_in = True
-        return render_template('home.html', user=user, logged_in=True)
-    else:
-        logged_in = False
-        return render_template('home.html', logged_in=False)
+# @app.route('/')
+# @app.route('/home')
+# def home():
+#     if 'email' in session:
+#         user = User.query.filter_by(email=session['email']).first()
+#         logged_in = True
+#         return render_template('home.html', user=user, logged_in=True)
+#     else:
+#         logged_in = False
+#         return render_template('home.html', logged_in=False)
     
 
 @app.route('/api/register', methods=['GET','POST'])
 def register():
     signup = SignUpForm()
-    logged_in = False
+    # logged_in = False
 
     if signup.validate_on_submit():
         if request.method == 'POST':
@@ -54,10 +54,9 @@ def register():
             existing_user = users.find_one({'email': request.signup['email']})
         #existing_user = User.query.filter_by(email=signup.email.data).first()
         if existing_user:
-            flash('Email already exists. Please use a different email.')
-            return redirect(url_for('register'))
-        elif '.cuny.edu' not in signup.email.data:
-            flash('Email must be a .cuny.edu email.')
+            print("temp")
+            # flash('Email already exists. Please use a different email.')
+            # return redirect(url_for('register'))
         else:
             user_id = getUsername(signup)
             new_user = {
@@ -68,53 +67,44 @@ def register():
                 'password': signup.password.data
             }
             users_collection.insert_one(new_user)
-            '''
-                user = User(username=user_id,
-                        first_name=signup.first_name.data,
-                        last_name=signup.last_name.data,
-                        email=signup.email.data,
-                        password=signup.password.data)
-            '''
-            logged_in = True
-#            db.session.add(user)
-#            db.session.commit()
-            flash(f'Account created for {signup.first_name.data}!')
-            session['email'] = signup.email.data
-            return redirect(url_for('home'))
-    return render_template('register.html', signup=signup, signin=signin, logged_in=logged_in)
+            # logged_in = True
+            # flash(f'Account created for {signup.first_name.data}!')
+            # session['email'] = signup.email.data
+            return redirect(url_for('home'))  # ?
+    return render_template('register.html', signup=signup, signin=signin, logged_in=logged_in)  # ?
 
 @app.route('/login', methods = ['GET'])
 def login():  
     signin = SignInForm()
     return ''
 
-@app.route('/api/forum', methods=['GET'])
-def forum():
-    if 'email' in session:
-        logged_in = True
-    else:
-        logged_in = False
-    form = ForumPost()
-    if form.validate_on_submit():
-        post_body = form.body.data
-    return render_template('forum.html', form=form, logged_in=logged_in)
+# @app.route('/api/forum', methods=['GET'])
+# def forum():
+#     # if 'email' in session:
+#     #     logged_in = True
+#     # else:
+#     #     logged_in = False
+#     form = ForumPost()
+#     if form.validate_on_submit():
+#         post_body = form.body.data
+#     return render_template('forum.html', form=form, logged_in=logged_in)
 
-@app.route('/api/users', methods=['GET'])
-def getUsername(input_form):
-    user_email = input_form.email.data  # get entire email
-    username = ''
-    i = 0
-    char = user_email[0]
+# @app.route('/api/users', methods=['GET'])
+# def getUsername(input_form):
+#     user_email = input_form.email.data  # get entire email
+#     username = ''
+#     i = 0
+#     char = user_email[0]
     
-    while char != '@':
-        char = user_email[i]
-        username += char
-        i += 1
+#     while char != '@':
+#         char = user_email[i]
+#         username += char
+#         i += 1
     
-    return jsonify(username)
-'''
-if __name__ == "__main__":
-    app.run(post=5000)
-'''
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+#     return jsonify(username)
+# '''
+# if __name__ == "__main__":
+#     app.run(post=5000)
+# '''
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0')
