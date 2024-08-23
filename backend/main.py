533676b06b910f8a347988f4aa3fc66f3f5195ec
alyliann <92405765+alyliann@users.noreply.github.com>
2024-08-22 23:06:51 -0400

    added login app route and create_username parser

--- backend/main.py
+++ backend/main.py
@@ -25,17 +25,42 @@ def users():
 
 @app.route('/api/login', methods=['GET', 'POST'])
 def login():
-    data = request.get_json()
-    print(data)
-    return data
+    data = request.get_json()  # data is an array of [email, password]
+
+    # do something here to check if email exists in database
+
+    # print(data)
+    return data  # possibly return boolean? True if existing, else False
 
 
 @app.route('/api/register', methods=['GET', 'POST'])
 def register():
-    data = request.get_json()
+    data = request.get_json()  # data is an array of [fname, lname, email, password]
     # print('Hello, World!')  # Print for testing
+
+    username = create_username(data[2])
+    print(username)
+
+    # do something here to check if user (or email) exists in database
+
+    # do something here to add user to database
+
     print(data)
-    return data
+    return data # possibly return boolean, or array with a boolean and a string for username
+
+
+# create username by parsing through user email:
+def create_username(email):
+    username = ''
+    i = 0
+    char = email[0]
+
+    while char != '@':
+        username += char
+        i += 1
+        char = email[i]
+
+    return username
 
 
 if __name__ == "__main__":
