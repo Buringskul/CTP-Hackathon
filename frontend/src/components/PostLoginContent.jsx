0333c7e1026a1b770e5c6d08ce1e07a2a9f5a6f7
Buringskul <simonaisakova6@gmail.com>
2024-08-23 15:16:59 -0400

    fixed up grid layout in postlogin page

--- frontend/src/components/PostLoginContent.jsx
+++ frontend/src/components/PostLoginContent.jsx
@@ -8,27 +8,27 @@ function PostLoginContent() {
         <div className="post-login-content">
             <h1>Welcome to Your Dashboard</h1>
             <div className="grid-container">
-                <a href='/discussionboard' className="grid-item">
+                <a href='/discussionboard' className="grid-items">
                     <FontAwesomeIcon className='icon' icon={faPencil} />
                     <h2>Academics</h2>
                 </a>
-                <a href='/discussionboard' className="grid-item">
+                <a href='/discussionboard' className="grid-items">
                     <FontAwesomeIcon className='icon' icon={faLeaf} />
                     <h2>Environment Awareness</h2>
                 </a>
-                <a href='/discussionboard' className="grid-item">
+                <a href='/discussionboard' className="grid-items">
                     <FontAwesomeIcon className='icon' icon={faHandHoldingHeart} />
                     <h2>Mental Health</h2>
                 </a>
-                <a href='/discussionboard' className="grid-item">
+                <a href='/discussionboard' className="grid-items">
                     <FontAwesomeIcon className='icon' icon={faShieldHeart} />
                     <h2>Safety</h2>
                 </a>
-                <a href='/discussionboard' className="grid-item">
+                <a href='/discussionboard' className="grid-items">
                     <FontAwesomeIcon className='icon' icon={faQuestion} />
                     <h2>Questions</h2>
                 </a>
-                <a href='/discussionboard' className="grid-item">
+                <a href='/discussionboard' className="grid-items">
                     <FontAwesomeIcon className='icon' icon={faTreeCity} />
                     <h2>CUNY Resources</h2>
                 </a>
