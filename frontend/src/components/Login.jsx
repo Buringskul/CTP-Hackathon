8419dc6ac885bc79d19a2d8d792ea056f6860127
alyliann <92405765+alyliann@users.noreply.github.com>
2024-08-22 23:06:30 -0400

    Added axios post request

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import '../styles/Login.css'; // If you have custom styles for the Login component
+import axios from 'axios';
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -33,10 +34,22 @@ function Login() {
             required
           />
         </div>
-        <button style={{ backgroundColor: '#555' }} type="submit">Login</button>
+        <button style={{ backgroundColor: '#555' }} type="submit" onClick={() => { check_user(email, password) }}>Login</button>
       </form>
     </div>
   );
+
+  function check_user(email, password) {
+    const returning_user = [email, password];
+    axios.post("http://localhost:5000/api/register", returning_user)
+      // axios.post("http://127.0.0.1:5000/api/register", new_user)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } 
 
   
 }
